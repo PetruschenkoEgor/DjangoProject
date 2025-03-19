76b8b353ed3d8bf9c134123c407494d453ca6433
Egor Petruschenko <jaguar.94@mail.ru>
2025-03-19 13:23:28 +0600

    добавил воркфлоу

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 import os
+import sys
+
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -165,3 +167,11 @@ CACHES = {
         'LOCATION': 'redis://redis:6379/1',
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'test_db.sqlite3',
+        }
+    }
